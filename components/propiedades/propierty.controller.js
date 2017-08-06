8b50a9610a3aa77fd657567334ae6378bd9432f7
Kevin <kvegac@ucenfotec.ac.cr>
2017-08-05 20:54:36 -0600

    captura de datos 2.0

--- components/propiedades/propierty.controller.js
+++ components/propiedades/propierty.controller.js
@@ -8,29 +8,39 @@
   function propiertyController(propiertyService,registerService) {
     var vm = this;
     vm.property = {};
+    vm.selected = {};
 
     function init() {
       vm.properties = propiertyService.getPropierties();
       vm.property = {};
+      vm.selected = {};
       vm.players = registerService.getPlayers();
     }
     init();
 
 
-    vm.buy = function(propiedad, jugador){
+    vm.buy = function(propiedad){
+      $("#formBuy").modal();
+      keepProperty(propiedad);
+		}
+
+    function keepProperty(propiedad) {
+      vm.selected = {};
+      vm.selected = propiedad;
+    }
+
+    vm.confirm = function(jugador) {
       var playerList = registerService.getPlayers();
       var player = '';
 
       for (var i = 0; i < playerList.length; i++) {
-        if (jugador === playerList[i].code) {
+        if (jugador === playerList[i].name) {
           player = playerList[i];
         }
       }
-      console.log(propiedad);
-
+      console.log(vm.selected);
       console.log(player);
-
-		}
+    }
 
   }
 })();
